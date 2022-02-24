90c2559c768d6d642bd3fa55a31711b863b00686
Yudi Kubota <yudikubota@gmail.com>
2022-02-24 11:03:15 -0300

    Remove chainlink references

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,18 +2,13 @@
 
 pragma solidity ^0.6.6;
 
-import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
 contract Lottery is VRFConsumerBase, Ownable {
     address payable[] public players;
     address payable public recentWinner;
     uint256 public randomness;
     uint256 public usdEntryFee;
-    uint256 public fee; // quanto LINK pagar para chainlink
-    bytes32 public keyHash; // para identificar o nodo na chainlink
-    AggregatorV3Interface internal ethUsdPriceFeed;
 
     event RequestedRandomness(bytes32 requestId);
 
@@ -25,18 +20,9 @@ contract Lottery is VRFConsumerBase, Ownable {
 
     LOTTERY_STATE public lottery_state;
 
-    constructor(
-        address _priceFeedAddress,
-        address _vrfCoordinator,
-        address _link,
-        uint256 _fee,
-        bytes32 _keyHash
-    ) public VRFConsumerBase(_vrfCoordinator, _link) {
+    constructor() public {
         usdEntryFee = 50 * 1e18;
-        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
         lottery_state = LOTTERY_STATE.CLOSED;
-        fee = _fee;
-        keyHash = _keyHash;
     }
 
     function enter() public payable {
@@ -67,7 +53,7 @@ contract Lottery is VRFConsumerBase, Ownable {
         // manipulados
 
         lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
-        bytes32 requestId = requestRandomness(keyHash, fee);
+        bytes32 requestId = requestRandomness(keyHash, oracleFee);
         emit RequestedRandomness(requestId);
     }
 
